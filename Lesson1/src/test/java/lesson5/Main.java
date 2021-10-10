01ab582df45d17e1a23a2248528a52f01e3baf5a
Alex <sam0khinaa@yandex.ru>
2021-10-11 01:35:21 +0300

    Урок 5.Введение в ООП

--- Lesson1/src/test/java/lesson5/Main.java
+++ Lesson1/src/test/java/lesson5/Main.java
@@ -1,2 +1,8 @@
-package lesson5;public class Main {
+package lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+        OldEmployee employee = new OldEmployee();
+        employee.showOld();
+    }
 }
--- Lesson1/src/test/java/lesson5/OldEmployee.java
+++ Lesson1/src/test/java/lesson5/OldEmployee.java
@@ -1,2 +1,19 @@
-package lesson5;public class OldEmployee {
+package lesson5;
+
+public class OldEmployee {
+    public static void showOld() {
+        Employee[] persArray = new Employee[5];
+        persArray[0] = new Employee("Ivanov Ivan", "cott@mail.ru", "Engineer", "892312312", 30, 30000);
+        persArray[1] = new Employee("Petrov Fedor", "Accountant", "Accountant", "892312311", 55, 47000);
+        persArray[2] = new Employee("Sidorov Andrey", "yuu@mailbox.com", "Manager", "89996785656", 34, 40000);
+        persArray[3] = new Employee("Ivanov Alexander", "demon@mailbox.com", "Driver", "89238987867", 56, 36000);
+        persArray[4] = new Employee("Solovyeva Marina", "Archivist", "Archivist", "89228765678", 60, 20000);
+
+        for (int i = 0; i < persArray.length; i++) {
+            Employee employee = persArray[i];
+            if (employee.getAge() > 40) {
+               employee.info();
+            }
+        }
+    }
 }
