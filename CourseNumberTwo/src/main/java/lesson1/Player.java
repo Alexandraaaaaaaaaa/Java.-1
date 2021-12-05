b200b72a202502abfb70b6cd2d81bea7576e02a9
Alex <sam0khinaa@yandex.ru>
2021-12-05 23:30:41 +0300

    Lesson2

---
+++ CourseNumberTwo/src/main/java/lesson1/Player.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class Players {
+    public String name;
+    public int speed;
+
+    public Players(){
+        name =
+    }
+}
---
+++ Lesson1/src/test/java/lesson7/Plate.java
@@ -0,0 +1,12 @@
+package lesson7;
+
+public class Plant {
+    private int food;
+    public Plate(int food) {
+        this.food = food;
+    }
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+}
+}
