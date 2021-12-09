855721f4885b969a82565ab573ce1dcec725b073
Alex <sam0khinaa@yandex.ru>
2021-12-09 21:17:33 +0300

    доделала

--- CourseNumberTwo/src/test/java/lesson3/Box.java
+++ CourseNumberTwo/src/test/java/lesson3/Box.java
@@ -7,6 +7,17 @@ public class Box
 {
     ArrayList<Fruit> fruits = new ArrayList<Fruit>();
 
+    public void mixFruit(int i1, int i2){
+        Fruit fruit1 = fruits.get(i1);
+        Fruit fruit2 = fruits.get(i2);
+
+        fruits.remove(i1);
+        fruits.remove(i2);
+
+        fruits.add(i1, fruit2);
+        fruits.add(i2, fruit1);
+    }
+
     public void addFruit(Fruit fruit)
     {
         if (fruits.isEmpty()){
@@ -20,4 +31,46 @@ public class Box
             fruits.add(fruit);
         }
     }
+
+
+    public int getWeight()
+    {
+        if (fruits.isEmpty()) {
+            return 0;
+        }
+
+        Fruit fruit = fruits.get(0);
+
+        return fruit.getWeight() * fruits.size();
+    }
+
+    public boolean compare(Box box)
+    {
+        return this.getWeight() == box.getWeight();
+    }
+
+    public String getTypeInBox(){
+        return fruits.get(0).getType();
+    }
+
+    public void pourOver(Box box)
+    {
+        if (fruits.isEmpty()) {
+            return;
+        }
+
+        if (box.getWeight() != 0 && this.getTypeInBox() != box.getTypeInBox()) {
+            return;
+        }
+
+        Fruit[] array = new Fruit[fruits.size()];
+        fruits.toArray(array);
+
+        for (int i = 0; array.length > i; i++){
+            box.addFruit(array[i]);
+        }
+
+        fruits.clear();
+    }
 }
+
--- CourseNumberTwo/src/test/java/lesson3/Main.java
+++ CourseNumberTwo/src/test/java/lesson3/Main.java
@@ -1,6 +1,18 @@
 package lesson3;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Apple apple = new Apple();
+        Apple apple1 = new Apple();
+
+        Box box = new Box();
+        Box box1 = new Box();
+
+        box.addFruit(apple);
+        box1.addFruit(apple1);
+
+        box.pourOver(box1);
+        System.out.println(box.getWeight());
+        System.out.println(box1.getWeight());
     }
 }
